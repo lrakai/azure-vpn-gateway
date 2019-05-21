9656e5e52fa36209f2a9fafde4d8ae6ef13d1c16
Logan Rakai <lmrakai@gmail.com>
2019-05-21 14:38:54 -0600

    Initial commit

---
+++ Variables.ps1
@@ -0,0 +1,7 @@
+$Lab="vpn-gw"
+$Region="WestUS"
+$User="student-lab-01@labscloudacademy.onmicrosoft.com"
+$Pass="labAccount1"
+$PolicyDefinitionName="lab-policy"
+$PolicyAssignmentName="lab-policy-assignment"
+$RoleDefinitionName="Lab Role"
